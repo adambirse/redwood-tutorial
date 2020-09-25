57dc1c478c194e52ef4dbc8eff982ee8461d3621
adambirse <abirse@worth.systems>
2020-09-25 11:20:18 +0100

    contact page form added.

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -1,5 +1,12 @@
 import BlogLayout from 'src/layouts/BlogLayout'
-import { Form, TextField, TextAreaField, Submit } from '@redwoodjs/forms'
+import {
+  Form,
+  TextField,
+  TextAreaField,
+  Submit,
+  FieldError,
+  Label,
+} from '@redwoodjs/forms'
 
 const ContactPage = () => {
   const onSubmit = (data) => {
@@ -8,13 +15,43 @@ const ContactPage = () => {
 
   return (
     <BlogLayout>
-      <Form onSubmit={onSubmit}>
-        <label htmlFor="name">Name</label>
-        <TextField name="name" />
-        <label htmlFor="email">Email</label>
-        <TextField name="email" />
-        <label htmlFor="message">Message</label>
-        <TextAreaField name="message" />
+      <Form onSubmit={onSubmit} validation={{ mode: 'onBlur' }}>
+        <Label name="name" errorClassName="error">
+          Name
+        </Label>
+        <TextField
+          name="name"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="name" className="error" />
+
+        <Label name="email" errorClassName="error">
+          Email
+        </Label>
+        <TextField
+          name="email"
+          validation={{
+            required: true,
+            pattern: {
+              value: /[^@]+@[^.]+\..+/,
+              message: 'Please enter a valid email address',
+            },
+          }}
+          errorClassName="error"
+        />
+        <FieldError name="email" className="error" />
+
+        <Label name="message" errorClassName="error">
+          Message
+        </Label>
+        <TextAreaField
+          name="message"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="message" className="error" />
+
         <Submit>Save</Submit>
       </Form>
     </BlogLayout>
